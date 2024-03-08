ca5d7ad46d99e0731284169b777b1555b5ddf72c
Ricardo Magalhães <ricardomagalhaes.dev@gmail.com>
2024-03-08 13:46:18 +0000

    Foi adicionado o metodo a classe Program

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1>>5); i = i <<1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
